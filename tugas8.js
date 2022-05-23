a7efaed807993712b4ec5d2048238a43838e7212
novifitri <animangap@gmail.com>
2022-05-23 20:12:41 +0700

    init

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+//buat object bebas
+let kucing = {
+    warna : "hitam",
+    nama : "abyu",
+    umur : 15
+}
+console.log("Nilai awal object kucing:")
+console.log(kucing)
+
+kucing.umur = 17
+console.log("Nilai setelah diubah object kucing:" )
+console.log(kucing)
